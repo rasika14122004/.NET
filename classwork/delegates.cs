116851daab940bb640766bd1e2c7714c2a8dc99d
RasikaKhade <rasikaskhade007@gmail.com>
2026-04-26 21:49:49 +0530

    first commit~

---
+++ classwork/delegates.cs
@@ -0,0 +1,6 @@
+﻿Build started at 10:50 PM...
+1>------ Build started: Project: delegate, Configuration: Debug Any CPU ------
+1>  Skipping analyzers to speed up the build. You can execute 'Build' or 'Rebuild' command to run analyzers.
+1>  delegate -> C:\Users\P C SOLUTION\source\repos\delegate\delegate\bin\Debug\net10.0\delegate.dll
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Build completed at 10:51 PM and took 38.733 seconds ==========
